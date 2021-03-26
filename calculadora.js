8cc97fcb704fe6df872a46414faec6b867222a8c
Álter José dos Santos Pereira <alterjose10@gmail.com>
2021-03-26 20:28:42 -0300

    teste unitário em JS

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+function soma(x, y) {
+    return x+y
+}
+
+function subtrair(x, y) {
+    return x-y
+}
+
+function multiplicar(x, y) {
+    return x*y
+}
+
+function dividir(x, y) {
+    return x/y
+}
